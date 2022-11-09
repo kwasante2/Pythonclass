4296a1e16b713ccb2da0cc3410da7213e4a27588
kwasante2 <77413812+kwasante2@users.noreply.github.com>
2022-11-09 12:50:53 -0500

    Add files via upload

---
+++ Listswithinlists.py
@@ -0,0 +1,23 @@
+fruits = ['apples','bananas', 'grapes', 'oranges', 'pears']
+print(fruits[0])
+print (fruits[1])
+print(fruits [2:5])
+fruits[2] = "kiwi"
+print (fruits)
+fruits.append ("strawberries")
+print(fruits)
+fruits.insert(2,"cantalope")
+print(fruits)
+fruits.remove("cantalope")
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruit_length = len(fruits)
+print(fruit_length)
+vegetables = ['carrots', 'corn', 'onions', 'peas']
+produce = fruits + vegetables 
+print(produce)
+fruits =["bananas",["Gala", "Empire", "McIntosh", "Golden Delicious"], "kiwi", "oranges", "pears"]
+print(fruits [1])
